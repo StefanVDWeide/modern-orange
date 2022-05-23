b9a85b4277dd1bfce29e75a14ed993caad5d8543
stefanvdWeide <stefan.vander.weide@hotmail.com>
2022-05-23 21:23:54 +0200

    Updated error styling

--- error.vue
+++ error.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-center text-center min-h-screen">
+  <div class="flex items-center justify-center min-h-screen">
     <div>
       <div v-if="error.statusCode === '404'" class="mb-4">
         <h1 class="text-6xl font-semibold">404 - Page not found!</h1>
@@ -9,21 +9,23 @@
           {{ error.statusCode }} - Internal error!
         </h1>
       </div>
-      <button
-        @click="handleError"
-        class="
-          px-6
-          py-3
-          text-xl
-          font-medium
-          text-white
-          bg-orange-600
-          rounded-md
-          hover:bg-orange-700
-        "
-      >
-        Back
-      </button>
+      <div class="flex justify-center">
+        <button
+          @click="handleError"
+          class="
+            px-6
+            py-3
+            text-xl
+            font-medium
+            text-white
+            bg-orange-600
+            rounded-md
+            hover:bg-orange-700
+          "
+        >
+          Back
+        </button>
+      </div>
     </div>
   </div>
 </template>
