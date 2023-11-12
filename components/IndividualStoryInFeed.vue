e69a57510679878eaeae934448d90f990c041583
StefanVDWeide <stefan.vander.weide@hotmail.com>
2023-11-12 19:37:25 +0100

    Added target _blank to comment button

--- components/IndividualStoryInFeed.vue
+++ components/IndividualStoryInFeed.vue
@@ -2,18 +2,17 @@
   <div v-if="cleanStoryObject" class="mb-6">
     <div class="inline-block mr-4 align-middle mb-4 md:mb-0">
       <a :href="cleanStoryObject.storyURL" target="_blank">
-        <div v-if="previewImageURL === 'standard' || imgError"
-          class="
-                                                                                                                                      flex
-                                                                                                                                      rounded-md
-                                                                                                                                      bg-orange-700
-                                                                                                                                      h-24
-                                                                                                                                      w-24
-                                                                                                                                      text-white text-6xl
-                                                                                                                                      font-semibold
-                                                                                                                                      justify-center
-                                                                                                                                      items-center
-                                                                                                                                    ">
+        <div v-if="previewImageURL === 'standard' || imgError" class="
+              flex
+              rounded-md
+              bg-orange-700
+              h-24
+              w-24
+              text-white text-6xl
+              font-semibold
+              justify-center
+              items-center
+            ">
           <span>{{
             cleanStoryObject.formattedURL.charAt(0).toUpperCase()
           }}</span>
@@ -26,21 +25,20 @@
       <div class="mb-2">
         <a href="">
           {{ cleanStoryObject.storyRanking }}.
-          <span
-            class="
-                                                                                                                              inline-flex
-                                                                                                                              items-center
-                                                                                                                              px-3
-                                                                                                                              py-0.5
-                                                                                                                              rounded-full
-                                                                                                                              text-sm
-                                                                                                                              font-medium
-                                                                                                                              leading-5
-                                                                                                                              bg-orange-100
-                                                                                                                              text-orange-800
-                                                                                                                            ">{{
-                                                                                                                              cleanStoryObject.storyType
-                                                                                                                            }}</span>
+          <span class="
+            inline-flex
+            items-center
+            px-3
+            py-0.5
+            rounded-full
+            text-sm
+            font-medium
+            leading-5
+            bg-orange-100
+            text-orange-800
+          ">{{
+            cleanStoryObject.storyType
+          }}</span>
         </a>
       </div>
       <a :href="cleanStoryObject.storyURL" target="_blank" class="text-lg font-semibold">
@@ -50,15 +48,14 @@
           <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
             x="0px" y="0px" viewBox="0 0 217.408 217.408" style="enable-background: new 0 0 217.408 217.408"
             xml:space="preserve" width="20px" height="20px" class="fill-gray-400 inline-block align-middle">
-            <path
-              d="M194.078,22.682c-10.747-8.193-22.606-12.348-35.248-12.348c-15.951,0-33.181,6.808-50.126,19.754
-                                                                                                                                    C91.759,17.142,74.529,10.334,58.578,10.334c-12.642,0-24.501,4.155-35.248,12.348C7.606,34.671-0.24,49.8,0.006,67.648
-                                                                                                                                    c0.846,61.117,100.093,133.233,104.317,136.273l4.381,3.153l4.381-3.153c4.225-3.04,103.472-75.156,104.317-136.273
-                                                                                                                                    C217.648,49.8,209.802,34.671,194.078,22.682z M153.833,149.017c-18.374,18.48-36.915,33.188-45.129,39.453
-                                                                                                                                    c-8.214-6.265-26.755-20.972-45.129-39.453c-31.479-31.661-48.274-59.873-48.57-81.585c-0.178-13.013,5.521-23.749,17.421-32.822
-                                                                                                                                    c8.073-6.156,16.872-9.277,26.152-9.277c17.563,0,34.338,10.936,45.317,20.11l4.809,4.018l4.809-4.018
-                                                                                                                                    c10.979-9.174,27.754-20.11,45.317-20.11c9.28,0,18.079,3.121,26.152,9.277c11.9,9.073,17.599,19.809,17.421,32.822
-                                                                                                                                    C202.107,89.145,185.311,117.356,153.833,149.017z" />
+            <path d="M194.078,22.682c-10.747-8.193-22.606-12.348-35.248-12.348c-15.951,0-33.181,6.808-50.126,19.754
+              C91.759,17.142,74.529,10.334,58.578,10.334c-12.642,0-24.501,4.155-35.248,12.348C7.606,34.671-0.24,49.8,0.006,67.648
+              c0.846,61.117,100.093,133.233,104.317,136.273l4.381,3.153l4.381-3.153c4.225-3.04,103.472-75.156,104.317-136.273
+              C217.648,49.8,209.802,34.671,194.078,22.682z M153.833,149.017c-18.374,18.48-36.915,33.188-45.129,39.453
+              c-8.214-6.265-26.755-20.972-45.129-39.453c-31.479-31.661-48.274-59.873-48.57-81.585c-0.178-13.013,5.521-23.749,17.421-32.822
+              c8.073-6.156,16.872-9.277,26.152-9.277c17.563,0,34.338,10.936,45.317,20.11l4.809,4.018l4.809-4.018
+              c10.979-9.174,27.754-20.11,45.317-20.11c9.28,0,18.079,3.121,26.152,9.277c11.9,9.073,17.599,19.809,17.421,32.822
+              C202.107,89.145,185.311,117.356,153.833,149.017z" />
             <g></g>
             <g></g>
             <g></g>
@@ -78,7 +75,7 @@
           {{ cleanStoryObject.storyScore }}
         </li>
         <li class="inline-block text-gray-400">
-          <NuxtLink :to="cleanStoryObject.storyItemLink">
+          <NuxtLink :to="cleanStoryObject.storyItemLink" target="_blank">
             <svg width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
               class="fill-gray-400 inline-block align-middle">
               <path
