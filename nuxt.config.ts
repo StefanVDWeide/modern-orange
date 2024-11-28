375742625c8297050473e5c4e70385c64e2380ae
StefanVDWeide <stefan.vander.weide@hotmail.com>
2024-11-28 15:17:30 +0100

    Removed Google Analytics in favor of plausible

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,27 +2,35 @@ import { defineNuxtConfig } from "nuxt/config";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	modules: ["@nuxt/image", "@nuxtjs/tailwindcss"],
+  modules: [
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/plausible"
+  ],
 
-	runtimeConfig: {
-		maxStoriesPerFeedPage: "25",
-		public: {
-			apiBaseUrl: ""
-		}
-	},
+  runtimeConfig: {
+    maxStoriesPerFeedPage: "25",
+    public: {
+      apiBaseUrl: ""
+    }
+  },
 
-	css: ["~/assets/css/tailwind.css"],
+  css: ["~/assets/css/tailwind.css"],
 
-	build: {
-		postcss: {
-			postcssOptions: {
-				plugins: {
-					tailwindcss: {},
-					autoprefixer: {}
-				}
-			}
-		}
-	},
-
-	compatibilityDate: "2024-08-27"
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {}
+        }
+      }
+    }
+  },
+  plausible: {
+    // Prevent tracking on localhost
+    ignoredHostnames: ["localhost"],
+    apiHost: "https://stats.weidev.nl"
+  },
+  compatibilityDate: "2024-08-27"
 });
--- plugins/vue-gtag.client.ts
+++
@@ -1,9 +0,0 @@
-import VueGtag from 'vue-gtag-next'
-
-export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(VueGtag, {
-        property: {
-            id: 'G-CEH99N8BLN'
-        }
-    });
-})
