9715f8526b5ccba413b067b0379d5a0c94da608e
stefan van der Weide <stefanvanderweide@Mac.home>
2026-01-10 12:59:07 +0100

    Migrate using codemod

--- server/api/getIndividualComment.post.ts
+++ server/api/getIndividualComment.post.ts
@@ -1,38 +1,38 @@
 import { get, child } from "firebase/database";
-import { db } from "~~/utils/firebase.js"
-import { formatTime } from "~~/utils/formatTime";
+import { db } from "~/utils/firebase.js"
+import { formatTime } from "~/utils/formatTime";
 
 // Methods
 const fetchIndividualComment = async (itemID: string) => {
-    const snapshot = await get(child(db, `v0/item/${itemID}`))
-        .then((snapshot) => {
-            if (snapshot.exists()) {
-                return snapshot.val();
-            } else {
-                console.log("No data avaible");
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+  const snapshot = await get(child(db, `v0/item/${itemID}`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        return snapshot.val();
+      } else {
+        console.log("No data avaible");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-    return snapshot;
+  return snapshot;
 };
 
 
 export default defineEventHandler(async (event) => {
-    try {
-        const body = await readBody(event);
-        const itemID = body.itemID;
-        const storyObject = await fetchIndividualComment(itemID);
-        storyObject.time = formatTime(storyObject.time * 1000);
-        return storyObject;
+  try {
+    const body = await readBody(event);
+    const itemID = body.itemID;
+    const storyObject = await fetchIndividualComment(itemID);
+    storyObject.time = formatTime(storyObject.time * 1000);
+    return storyObject;
 
-    } catch (error) {
-        console.log("An error occured while retrieving an additional stories on the server");
-        console.log(error);
-        return {
-            error: true,
-        }
+  } catch (error) {
+    console.log("An error occured while retrieving an additional stories on the server");
+    console.log(error);
+    return {
+      error: true,
     }
+  }
 })
